4ae8f62ffa6d9cc2557c0d30f4f9bfdad6f32022
ybo <ybo@yonyou.com>
2021-09-23 09:33:27 +0800

    对组件进行微调

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,33 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  "parser": "vue-eslint-parser",
   parserOptions: {
+    "parser": "babel-eslint",
     sourceType: 'module'
   },
   env: {
     browser: true,
-    jquery: true
+    jquery: true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends:  [
+
+    "standard",
+
+    "plugin:vue/essential"
+
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue',
   ],
+  "settings": {
+    "html/html-extensions": [".html", ".we"],
+    "html/report-bad-indent": "error"
+  },
   // add your custom rules here
   'rules': {
     'object-curly-spacing': 0,
@@ -21,8 +35,15 @@ module.exports = {
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
-    "indent": [2, 2, {"SwitchCase": 1}],
+    //"indent": ["error", 2, {"SwitchCase": 1}],
+    "vue/script-indent": ["error", 2, {  // script标签缩进设置
+      "baseIndent": 0,
+      "switchCase": 0,
+      "ignores": []
+    }],
+    "vue/valid-template-root": 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+  },
+  overrides: [ { 'files': ['*.vue'], 'rules': { 'indent': 'off' } } ]
 }
